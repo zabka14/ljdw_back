a07d645b1cf7ab8c3ec1e5cf485a4a03eb768589
Benjamin Vianey <vianey.benjamin@gmail.com>
2024-06-24 14:00:54 +0200

    Test

--- api/auth.js
+++ api/auth.js
@@ -1,39 +1,41 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User'); // Assurez-vous d'avoir un modèle User
-
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ googleId: profile.id });
-    if (!user) {
-      user = new User({
-        googleId: profile.id,
-        displayName: profile.displayName,
-        emails: profile.emails
-      });
-      await user.save();
-    }
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-}));
+const User = require('../models/User');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
 });
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+async (token, tokenSecret, profile, done) => {
+  try {
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if (existingUser) {
+      return done(null, existingUser);
+    }
+
+    const newUser = new User({
+      googleId: profile.id,
+      displayName: profile.displayName,
+      emails: profile.emails // Stocke directement le tableau d'objets emails
+    });
+
+    await newUser.save();
+    done(null, newUser);
+  } catch (err) {
+    done(err, null);
+  }
+}
+));
 
 module.exports = passport;
--- models/User.js
+++ models/User.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  googleId: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  displayName: String,
-  emails: [String]
+  googleId: { type: String, required: true },
+  displayName: { type: String, required: true },
+  emails: [{
+    value: String,
+    verified: Boolean
+  }]
 });
 
 module.exports = mongoose.model('User', userSchema);
